2c081197aa49edf24e8776ac7c8707f14914dbd4
ManuelNC250 <manuelnavarrocortes@elpuig.xeill.net>
2025-10-09 18:48:50 +0200

    Initial commit

---
+++ src/main/java/com/EntroYA/EntroYa/EntroYaApplication.java
@@ -0,0 +1,13 @@
+package com.EntroYA.EntroYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntroYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntroYaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EntroYA/EntroYa/EntroYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EntroYA.EntroYa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntroYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
